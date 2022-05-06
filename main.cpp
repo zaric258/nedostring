f0d17e27da03756be0c3b426c15743f3942f000d
Kirill <Kirill@DESKTOP-8GNOR6F>
2022-05-06 15:29:30 +0300

    update2.0

--- main.cpp
+++ main.cpp
@@ -4,6 +4,21 @@ using namespace std;
 
 int main()
 {
+	setlocale(LC_ALL, "rus");
+	string line;
+	int counter = 0;
+	char letter;
 
+	cout << "Enter text: ";
+	getline(cin, line);
+	cout << "Enter letter: ";
+	cin >> letter;
+
+	for (unsigned int i = 0; i < line.length(); i++)
+		if (line[i] == letter)
+		{
+			counter++;
+		}
+	cout << "In the text " << counter << " letter" << endl;
 	return 0;
 }
